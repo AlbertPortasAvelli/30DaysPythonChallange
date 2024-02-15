8501b87cdcd0f8d1d85f372d5ecb44e4fa9ccfa6
Albert Portas Avelli <albertportasavelli@gmail.com>
2024-02-15 17:01:35 +0100

    23th 30DaysPython Challenge

---
+++ theory/day23virtual_environtments.py
@@ -0,0 +1,22 @@
+### Setting up Virtual Environments ###
+
+# A virtual environment is a self-contained directory tree that contains its own Python installation,
+# as well as its own copy of pip for installing packages.
+
+# To create a virtual environment named 'venv' in your project directory, run the following command:
+# python -m venv venv
+
+# After creating the virtual environment, activate it using the appropriate script in the 'bin' directory:
+# On Windows:
+#   venv\Scripts\activate
+# On Unix or MacOS:
+#   source venv/bin/activate
+
+# Once activated, your terminal or command prompt will show '(venv)' indicating the virtual environment is active.
+# Now, you can install packages using pip, and they will be isolated within the virtual environment.
+
+# For example, to install Flask, run:
+# pip install Flask
+
+# To deactivate the virtual environment and return to the global Python interpreter, simply run:
+# deactivate
